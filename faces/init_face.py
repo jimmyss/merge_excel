2ce6cdf8dbe781208437a048f4c55ecfcf6b713c
jimmy <3240429693@qq.com>
2022-08-15 17:55:48 +0800

    accomplish UI of bill_summary

--- faces/init_face.py
+++ faces/init_face.py
@@ -1,8 +1,6 @@
 import ttkbootstrap as ttk
 from ttkbootstrap.constants import *
-from faces import data_validation
-from faces import merge_excel
-from faces import revenue_division
+from faces import data_validation, merge_excel, revenue_division, bill_summary
 
 class init_face():
     def __init__(self, master):
@@ -12,8 +10,6 @@ class init_face():
         """
         self.master = master
         self.master.config()
-        #线程池
-        self.thread_list=[]
         # 基准界面initface
         self.initface = ttk.Frame(self.master, )
         self.initface.grid(row=0, column=0)
@@ -25,6 +21,8 @@ class init_face():
         task2_but.grid(row=2, column=1, sticky=E, pady=10, padx=10)
         task3_but = ttk.Button(self.initface, text='月度分账收入', command=self.to_revenue_division)
         task3_but.grid(row=3, column=1, sticky=E, padx=10, pady=10)
+        task4_but = ttk.Button(self.initface, text='月度账单汇总取值', command=self.to_bill_summary)
+        task4_but.grid(row=4, column=1, sticky=E, padx=10, pady=10)
 
     def to_merge_excel(self, ):
         self.initface.destroy()
@@ -38,5 +36,6 @@ class init_face():
         self.initface.destroy()
         revenue_division.revenue_division(self.master)
 
-    def get_thread_list(self):
-        return self.thread_list
+    def to_bill_summary(self):
+        self.initface.destroy()
+        bill_summary.bill_summary(self.master)
